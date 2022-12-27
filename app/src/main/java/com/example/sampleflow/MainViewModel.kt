ebe5b98ba2207ef7c3e7086fbedb03505cd6c281
mehdi <dehghani_m@rocketmail.com>
2022-12-27 11:57:01 +0330

    add corotuine library

--- app/src/main/java/com/example/sampleflow/MainViewModel.kt
+++ app/src/main/java/com/example/sampleflow/MainViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.sampleflow
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.flow
 
 class MainViewModel : ViewModel() {
+
+    val countDownFlow = flow<Int> {
+        val startin
+    }
 }
