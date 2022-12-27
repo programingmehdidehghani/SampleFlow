be542e41f6a857d310fb57da94b50fc28f37a264
mehdi <dehghani_m@rocketmail.com>
2022-12-27 14:51:29 +0330

    life cycle corotiune

--- app/src/main/java/com/example/sampleflow/MainActivity.kt
+++ app/src/main/java/com/example/sampleflow/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,25 +16,41 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sampleflow.ui.theme.SampleFlowTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             SampleFlowTheme {
                 val viewModel = viewModel<MainViewModel>()
-                val time = viewModel.countDownFlow.collectAsState(initial = 10)
+                val count = viewModel.countDownFlow.collectAsState(initial = 0)
                Box(modifier = Modifier.fillMaxSize()){
-                   Text(
-                      text = time.value.toString(),
-                      fontSize = 30.sp,
-                      modifier = Modifier.align(Alignment.Center)
-                   )
+                   Button(onClick = { viewModel.incrementCounter() }) {
+                       Text(text = "count: ${count.value}")
+                   }
                }
             }
         }
     }
+}
+
+fun ComponentActivity.collectLatestLifecycleFlow(flow: Flow<T>,collect: suspend (T) -> Unit) {
+     lifecycleScope.launch {
+         repeatOnLifecycle(Lifecycle.State.STARTED){
+               flow.collectLatest(collect)
+         }
+     }
 }
 
