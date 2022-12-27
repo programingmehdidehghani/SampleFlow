bfd34d20fa8714398c7eb3b7f3ad7cd38f78577a
mehdi <dehghani_m@rocketmail.com>
2022-12-27 12:02:02 +0330

    make emit flow

--- app/src/main/java/com/example/sampleflow/MainViewModel.kt
+++ app/src/main/java/com/example/sampleflow/MainViewModel.kt
@@ -1,11 +1,20 @@
 package com.example.sampleflow
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 
 class MainViewModel : ViewModel() {
 
     val countDownFlow = flow<Int> {
-        val startin
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(startingValue)
+        while (currentValue > 10){
+            delay(1000L)
+            currentValue--
+            emit(currentValue)
+
+        }
     }
 }
