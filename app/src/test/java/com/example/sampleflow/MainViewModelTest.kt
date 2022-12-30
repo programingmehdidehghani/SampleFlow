2d68ced654a48a30a3a2228a7f38fd121a6eb6d3
mehdi <dehghani_m@rocketmail.com>
2022-12-30 18:22:20 +0330

    write for test flow

--- app/src/test/java/com/example/sampleflow/MainViewModelTest.kt
+++ app/src/test/java/com/example/sampleflow/MainViewModelTest.kt
@@ -1,6 +1,10 @@
 package com.example.sampleflow
 
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 
 
 class MainViewModelTest {
@@ -10,5 +14,15 @@ class MainViewModelTest {
     @Before
     fun setUp() {
         viewModel = MainViewModel()
+    }
+
+    @Test
+    fun `countDownFlow, properly counts down from 5 to 0`() = runBlocking{
+        viewModel.countDownFlow.test {
+            for (i in 5 downTo 0){
+                val emission = awaitItem()
+                assertThat(emission).isEqualTo(i)
+            }
+        }
     }
 }
